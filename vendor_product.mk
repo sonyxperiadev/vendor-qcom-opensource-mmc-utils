6b1954657a38201d4e2cc48b62992a00943a60f4
Sachin Gupta <quic_sachgupt@quicinc.com>
2022-03-01 15:24:05 +0530

    ufs: config: Test script crashing on user build.
    The product package init.qti.ufs.rc is included for all build (user, userdebug, eng),
    but init.qti.ufs.debug.sh is included only for userdebug and eng build. So for user
    build, as part of init.qti.ufs.rc script when init.qti.ufs.debug.sh is executed , it
    will cause crash as init.qti.ufs.debug.sh is not included for the user build.
    
    Fixing this by defining init.qti.ufs.rc only for userdebug/eng so that init.qti.ufs.rc
    can only be executed for user build.
    
    Change-Id: I3efe0b4c41f1eeb1f8d0239bdd525c1421dd56a1

--- vendor_product.mk
+++ vendor_product.mk
@@ -1,8 +1,8 @@
 # UFS init scripts
-PRODUCT_PACKAGES += init.qti.ufs.rc
 
 # additional debugging on userdebug/eng builds
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+  PRODUCT_PACKAGES += init.qti.ufs.rc
   PRODUCT_PACKAGES += init.qti.ufs.debug.sh
 endif
 
